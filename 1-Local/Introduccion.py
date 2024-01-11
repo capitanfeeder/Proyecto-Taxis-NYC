8ae3616b54b0b1003510b2b6e9fe0cb3687c4807
Gabriel Sosa <gabrielwesker26@gmail.com>
2024-01-11 20:51:53 -0300

    Create Introduccion.py

---
+++ 1-Local/Introduccion.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+# Título de la página
+st.title("Quiénes somos: datagnius")
+
+# Separador visual
+st.markdown('***')
+
+# Imagen de la empresa
+st.image("Datasets/4-Extras/datagnius_jpg.jpg", use_column_width=True)
+
+# Introducción sobre Datagnius
+st.markdown('## Datagnius - Transformando Datos en Decisiones Rentables')
+
+st.write("""
+Datagnius es una consultora especializada en análisis, ciencia e ingeniería de datos, dedicada a potenciar decisiones estratégicas que maximicen las ganancias de nuestros clientes. Con un compromiso inquebrantable hacia la excelencia y el perfeccionismo en los detalles, hemos liderado proyectos exitosos en diversos sectores.
+
+Impulsados por nuestra visión de un futuro más sostenible y alineados con las tendencias del mercado, exploramos activamente la relación entre los datos y las oportunidades de negocio. Nuestra misión es proporcionar insights valiosos que permitan a las empresas tomar decisiones informadas y rentables.
+
+Contamos con un equipo de profesionales especializados en análisis de datos, machine learning y tecnologías cloud. Nos dedicamos a desbloquear el potencial de los datos para que empresas, como la tuya, avancen hacia el futuro con confianza y éxito.
+""")
+
+# Introducción sobre Datagnius
+st.markdown('## ¿Por qué elegir a datagnius?')
+
+st.write("""
+* **Experiencia:** contamos con más de 10 años de experiencia en el sector de la consultoría de datos teniendo como clientes a empresas de primer nivel gracias a nuestro bien detallado plan de trabajo.
+""")
